044c26995781e8eda660afa96a9abede61c9f829
SamuelInocencio <samuelferreirainocencio@gmamil.com>
2025-10-26 17:48:54 -0300

    DevBurguerAPI SecondModule

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,48 @@
+import bcrypt from 'bcrypt';
+import * as Yup from 'yup';
+import User from '../models/User.js';
+
+class SessionController {
+  async store(request, response) {
+    const schema = Yup.object({
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    const isValid = await schema.isValid(request.body, { strict: true });
+
+    const emailOrPasswordIsIncorrect = () => {
+      response.status(400).json({ error: 'Email or password incorrect.' });
+    };
+
+    if (!isValid) {
+      return emailOrPasswordIsIncorrect();
+    }
+
+    const { email, password } = request.body;
+
+    const existingUser = await User.findOne({ where: { email } });
+
+    if (!existingUser) {
+      return emailOrPasswordIsIncorrect();
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password_hash,
+    );
+
+    if (!isPasswordCorrect) {
+      return emailOrPasswordIsIncorrect();
+    }
+
+    return response.status(200).json({
+      id: existingUser.id,
+      name: existingUser.name,
+      email: existingUser.email,
+      admin: existingUser.admin,
+    });
+  }
+}
+
+export default new SessionController();
---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import Sequelize from 'sequelize';
+
+import Product from '../app/models/Product.js';
+import User from '../app/models/User.js';
+import configDatabase from '../config/database.cjs';
+
+const models = [User, Product];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(configDatabase);
+    models.map((model) => model.init(this.connection));
+  }
+}
+
+export default new Database();
